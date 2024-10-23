2f2870b1452ca453df02b731bb7130c903b8a556
rajkonkret <rajkonkret660@gmail.com>
2024-10-23 10:59:06 +0200

    [3.3] update warunki.py

--- day3/warunki.py
+++ day3/warunki.py
@@ -39,18 +39,35 @@ else:  # w przeciwnym wypadku
 
 # Indent Rainbow - plugin do pokazywania wcięć
 
-podatek = 0
-zarobki = int(input("Podaj zarobki"))
-if zarobki < 10_000:  # do 9999
-    podatek = 0
-elif zarobki < 30000:
-    podatek = 0.2
-elif zarobki < 100_000:
-    podatek = 0.4
-else:
-    podatek = 0.9
+# podatek = 0
+# zarobki = int(input("Podaj zarobki"))
+# if zarobki < 10_000:  # do 9999
+#     podatek = 0
+# elif zarobki < 30000:
+#     podatek = 0.2
+# elif zarobki < 100_000:
+#     podatek = 0.4
+# else:
+#     podatek = 0.9
+#
+# # jeden warunek spełniony wychodzimy z drzewa warunków
+# print(f"Podatek wynosi {podatek * zarobki}")
+# # dodac dla zarobków od 10000 do 29999 podatek 0.2
+# # zarobki > 9999 and  zarobki < 30000 -> 10000 < zarobki < 30000
 
-# jeden warunek spełniony wychodzimy z drzewa warunków
-print(f"Podatek wynosi {podatek * zarobki}")
-# dodac dla zarobków od 10000 do 29999 podatek 0.2
-# zarobki > 9999 and  zarobki < 30000 -> 10000 < zarobki < 30000
+suma_zam = 150
+if suma_zam > 100:
+    rabacik = 25
+else:
+    rabacik = 0
+print(f"Rabat wynosi", rabacik)  # Rabat wynosi 25
+
+rabat = 25 if suma_zam > 100 else 0
+print(f"Rabat wynosi", rabat)  # Rabat wynosi 25 ternary operator, operator warunkowy
+
+# zasymulujemy system zbierania logów
+# zmienne będą przechowywac nazwę systemu jaki przysłał log
+# email, console, inny (else)
+# console: "Stało się coś strasznego"
+# email "System email"
+# inny: "Nie znam takiego systemu"
