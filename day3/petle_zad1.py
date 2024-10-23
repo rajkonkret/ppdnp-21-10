588e24c003fd3e1f5ed724bb2cfb9521ec04d3fc
rajkonkret <rajkonkret660@gmail.com>
2024-10-23 15:15:04 +0200

    [3.10] update petle_zad1.py

--- day3/petle_zad1.py
+++ day3/petle_zad1.py
@@ -100,7 +100,7 @@ for i in range(-10, 0):
 # spam %= 1    spam = spam % 1
 for c in lista3:
     if c == 2:
-        c += 1 # c = c + 1
+        c += 1  # c = c + 1
         print("tylko dla c=2")
         print(c)
     print("Przy każdym przejsciu pętli")
@@ -115,3 +115,106 @@ print("Po zakońćzeniu pętli")
 # Przy każdym przejsciu pętli
 # Po zakońćzeniu pętli
 
+imiona = ["Radek", 'Tomek', 'Zenek', "Karolina"]
+print(imiona)
+print(type(imiona))
+# ['Radek', 'Tomek', 'Zenek', 'Karolina']
+# <class 'list'>
+
+for p in imiona:
+    print(p)
+# Radek
+# Tomek
+# Zenek
+# Karolina
+
+# wypisac elementy z listy z indeksem
+# 0 Radek
+# 1 Tomek...
+for i in range(len(imiona)):  # range(4) -> 0123
+    print(i, imiona[i])
+# 0 Radek
+# 1 Tomek
+# 2 Zenek
+# 3 Karolina
+
+for p in imiona:
+    print(imiona.index(p), p)
+# 0 Radek
+# 1 Tomek
+# 2 Zenek
+# 3 Karolina
+
+# enumerate() - zwraca element kolekcji i numer indeksu
+for p in enumerate(imiona):
+    print(p)
+# (0, 'Radek') -> 0 Radek
+# (1, 'Tomek')
+# (2, 'Zenek')
+# (3, 'Karolina')
+a, b = (3, 'Karolina')
+print(a, b)  # 3 Karolina
+
+for i, o in enumerate(imiona):
+    print(i, o)
+# 0 Radek
+# 1 Tomek
+# 2 Zenek
+# 3 Karolina
+
+for i, o in enumerate(imiona, start=1):
+    print(i, o)
+# 1 Radek
+# 2 Tomek
+# 3 Zenek
+# 4 Karolina
+
+imiona = ["Radek", 'Tomek', 'Zenek', "Karolina"]
+wiek = [44, 55, 32, 27]
+
+# wypisac to tak Radek 44
+for i in imiona:
+    print(i, wiek[imiona.index(i)])
+# Radek 44
+# Tomek 55
+# Zenek 32
+# Karolina 27
+imiona = ["Radek", 'Tomek', 'Zenek', "Karolina", "Anna"]
+wiek = [44, 55, 32, 27]
+# for i in imiona:
+#     print(i, wiek[imiona.index(i)]) # IndexError: list index out of range
+
+# zip() - łączy kolekcje w jedną, te które mają wszystkie dane
+for i in zip(imiona, wiek):
+    print(i)
+# ('Radek', 44)
+# ('Tomek', 55)
+# ('Zenek', 32)
+# ('Karolina', 27)
+for o, w in zip(imiona, wiek):
+    print(o, w)
+# Radek 44
+# Tomek 55
+# Zenek 32
+# Karolina 27
+
+# 0 Radek 44
+for i in enumerate(zip(imiona, wiek)):
+    print(i)
+# (0, ('Radek', 44))
+# (1, ('Tomek', 55))
+# (2, ('Zenek', 32))
+# (3, ('Karolina', 27))
+a, b = (0, ('Radek', 44))
+print(a, b)  # 0 ('Radek', 44)
+c, d = ('Radek', 44)
+print(c, d)  # Radek 44
+a, (c, d) = (3, ('Karolina', 27))
+print(a, c, d)  # 3 Karolina 27
+
+for i, (o, w) in enumerate(zip(imiona, wiek)):
+    print(i, o, w)
+# 0 Radek 44
+# 1 Tomek 55
+# 2 Zenek 32
+# 3 Karolina 27
