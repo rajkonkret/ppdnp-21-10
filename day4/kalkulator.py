8d08047c0c3007e3440fab8fb4f2711ad63e2854
rajkonkret <rajkonkret660@gmail.com>
2024-10-24 10:16:14 +0200

    [4.2] add kalkulator.py

---
+++ day4/kalkulator.py
@@ -0,0 +1,10 @@
+import math
+
+print(math.pi)  # 3.141592653589793
+
+print(math.cos(2))  # -0.4161468365471424
+degrees = 90
+radians = math.radians(degrees)
+
+print(radians)  # 1.5707963267948966
+print(math.sin(radians))  # 1.0
