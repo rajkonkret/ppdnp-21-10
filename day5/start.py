1a9b41e350ca059cd1bf3370de33a90f4ce9c29c
rajkonkret <rajkonkret660@gmail.com>
2024-10-25 12:13:19 +0200

    [5.8] update pakiet

--- day5/start.py
+++ day5/start.py
@@ -3,6 +3,17 @@
 # kl2.cz1.ruszaj()
 
 import pakiet
+from pakiet import fun  # import pliku z pakietu
+import pakiet.fun as pk  # import jako alias
 
 # po dodoaniu do pliku __init__ metoda jest widoczna
 pakiet.powitanie()  # Cześć
+
+# po zaimportowaniu pliku metoda jest widoczna
+fun.info()  # Jestem pakietem
+pk.info()  # Jestem pakietem
+pk.powitanie()  # Cześć
+# Cześć
+# Jestem pakietem
+# Jestem pakietem
+# Cześć
--- pakiet/fun.py
+++ pakiet/fun.py
@@ -1,2 +1,13 @@
 def powitanie():
     print("Cześć")
+
+
+def info():
+    print("Jestem pakietem")
+
+if __name__ == '__main__':
+    powitanie()
+    info()
+
+# Cześć
+# Jestem pakietem
